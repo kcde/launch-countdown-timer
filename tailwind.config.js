b7813ff22b703f67f3decdda8c42b88ae2ee35ef
keside <wilsonezeala@yahoo.com>
2021-04-24 07:43:45 +0100

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        desatBlue: 'hsl(236, 21%, 26%)',
+        darkBlue: 'hsl(235, 16%, 14%)',
+        veryDarkBlue: 'hsl(234, 17%, 12%)',
+        grayBlue: 'hsl(237, 18%, 59%)',
+        softRed: 'hsl(345, 95%, 68%)',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
